99bd15ea08a53bdf680cc647d99f5b2b5608181a
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-03-26 16:21:03 +0100

    länk till min email i footer

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,21 +1,25 @@
-import './Footer.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEarthAmericas, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Importera ikonerna i en enda rad
 
 const Footer = () => {
     const year = new Date().getFullYear();
     const name = 'FASHONHUB';
     const author = 'Ellinor Jansson Lande';
-  return (
-      <footer className='Footer'>
-          <p className='Footerp'>{name} &copy; {year} by {author}</p>
-          <p className='Footerp'><FontAwesomeIcon icon={faEarthAmericas} size="lg"/> Stockholm, Sweden</p>
-          <p className='Footerp'><FontAwesomeIcon icon={faPhone} size="lg"/> Phone: 08-123 456 78</p>
-          <p className='Footerp'><FontAwesomeIcon icon={faEnvelope} size="lg" /> Email:  Ellinor_jansson@hotmail.com </p>
-      </footer>
-  )
+    const email = " Ellinor_jansson@hotmail.com"; // Definiera e-post som en variabel för att undvika upprepning
+
+    return (
+        <footer className='Footer'>
+            <p className='Footerp'>{name} &copy; {year} by {author}</p>
+            <p className='Footerp'><FontAwesomeIcon icon={faEarthAmericas} size="lg"/> Stockholm, Sweden</p>
+            <p className='Footerp'><FontAwesomeIcon icon={faPhone} size="lg"/> Phone: 08-123 456 78</p>
+            {/* Gör e-postadressen till en klickbar "mailto" länk */}
+            <p className='Footerp'>
+                <FontAwesomeIcon icon={faEnvelope} size="lg"/> Email:   
+                <a href={`mailto:${email}`} className="FooterEmail">{email}</a>
+            </p>
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
